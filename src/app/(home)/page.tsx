1bacab6e72ba1830ccb57cda77a03d128c35c26c
Niksa Volarevic <nikssa@gmail.com>
2024-07-19 15:57:20 +0200

    Update homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-
-import { FormEvent, useState, ChangeEvent, useEffect } from 'react';
-import Input from '../components/formElements/TextField/TextField';
-
-import LoginIllustration from '/public/LoginIllustration.jpg';
-
-import './page.scss';
-import Image from 'next/image';
-import RadioGroup from '../components/formElements/RadioGroup/RadioGroup';
-import Button from '../components/formElements/Button/Button';
-
 const HomePage = () => {
   return <>HomePage</>;
 };
