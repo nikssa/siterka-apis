3715296458684826ba2da1832e2d83fd51d0a07b
nikssa <nikssa@gmail.com>
2024-09-09 16:53:35 +0200

    Testing nextjs built-in loading feature

--- src/app/(admin)/users/_loading.tsx
+++ src/app/(admin)/users/_loading.tsx
@@ -3,7 +3,3 @@ import React from 'react';
 export default function Loading() {
   return <div>Loading...</div>;
 }
-
-// export const Loading = () => {
-//   return <div>Loading...</div>;
-// };
--- src/app/(auth)/login/loading.tsx
+++ src/app/(auth)/login/loading.tsx
@@ -3,7 +3,3 @@ import React from 'react';
 export default function Loading() {
   return <div>Loading...</div>;
 }
-
-// export const loading = () => {
-//   return <div>loading...</div>;
-// };
