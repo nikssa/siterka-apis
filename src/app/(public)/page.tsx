08272c1a8a21774082f23597e587686d123177e6
nikssa <nikssa@gmail.com>
2024-09-09 18:48:49 +0200

    Testing fetch data directly from the server component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,19 +1,35 @@
 import FalseButton from '../components/FalseButton';
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 const HomePage = async () => {
-  const blogs = await fetch('https://api.vercel.app/blog', {
-    method: 'GET'
-  });
-  const blogsData = await blogs.json();
+  // const blogs = await fetch('https://api.vercel.app/blog', {
+  //   method: 'GET'
+  // });
+  // const blogsData = await blogs.json();
+
+  const users = await prisma.user.findMany();
+
+  console.log('users', users);
 
   return (
     <>
       <section>
         <h1>Home Page</h1>
-        <ol>
+        {/* <ol>
           {blogsData.map((blog: any) => (
             <li key={blog.id} style={{ listStyleType: 'number' }}>
               {blog.title} - {blog.author}
+            </li>
+          ))}
+        </ol> */}
+
+        <ol>
+          {users.map((user: any) => (
+            <li key={user.id} style={{ listStyleType: 'number' }}>
+              {user.name} - {user.email}
             </li>
           ))}
         </ol>
