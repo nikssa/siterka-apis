c406c58db5971e1944952718f60d6f850cb7099c
nikssa <nikssa@gmail.com>
2024-09-19 02:04:29 +0200

    Adding PostData types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,49 @@
+enum Age {
+  EIGHTEEN = 'EIGHTEEN',
+  TWENTY_FIVE = 'TWENTY_FIVE',
+  FORTY = 'FORTY',
+  FIFTY_FIVE = 'FIFTY_FIVE',
+  SIXTY_FIVE = 'SIXTY_FIVE'
+}
+
+enum ChildAge {
+  NEWBORNS = 'NEWBORNS', // 0-2 months
+  INFANTS = 'INFANTS', // INFANTS // 3-12 months
+  TODDLERS = 'TODDLERS', // TODDLERS // 1-2 years
+  PRESCHOOLERS = 'PRESCHOOLERS', // PRESCHOOLERS // 3-6 years
+  SCHOOLERS = 'SCHOOLERS', // SCHOOLERS // 7-12 years
+  TEENS = 'TEENS' // TEENS // 13-19 years
+}
+
+enum Education {
+  ELEMENTARY = 'ELEMENTARY',
+  HIGH_SCHOOL = 'HIGH_SCHOOL',
+  COLLEGE = 'COLLEGE',
+  GRADUATE = 'GRADUATE'
+}
+
+enum Languages {
+  ENGLISH = 'ENGLISH',
+  SPANISH = 'SPANISH',
+  FRENCH = 'FRENCH',
+  GERMAN = 'GERMAN',
+  ITALIAN = 'ITALIAN',
+  OTHER = 'OTHER'
+}
+
+enum EarningsRate {
+  HOURLY = 'HOURLY',
+  MONTHLY = 'MONTHLY',
+  YEARLY = 'YEARLY'
+}
+
 export type PhotoProps = {
+  id?: number;
   url: string;
+  author?: UserDataProps;
+  authorId?: number;
+  post?: PostDataProps;
+  postId?: number;
 };
 
 export type UserDataProps = {
@@ -29,9 +73,39 @@ export type ProfileDataProps = {
 
 export type PostDataProps = {
   id: number;
-  title: string;
-  content: string;
   createdAt: Date;
   updatedAt: Date;
+  published: boolean;
+  title: string;
+  description: string;
+  email: string;
+  phone: string;
+  birthDate: Date;
+  ageGroup: Age;
+  country: string;
+  city: string;
+  experience: number;
+  childAgeGroup: ChildAge;
+  numberOfChildren: number;
+  pets: Boolean;
+  specialNeeds: Boolean;
+  inOwnHouse: Boolean;
+  cleaning: Boolean;
+  cleaningChildOnly: Boolean;
+  shopping: Boolean;
+  cooking: Boolean;
+  cookingChildOnly: Boolean;
+  outWithChild: Boolean;
+  car: Boolean;
+  driversLicense: Boolean;
+  smoking: Boolean;
+  education: Education;
+  sitterCourse: Boolean;
+  firstAid: Boolean;
+  languages: Languages;
+  otherLanguages: string;
+  earnings: number;
+  earningsRate: EarningsRate;
+  photos?: PhotoProps[];
   author?: UserDataProps;
 };
