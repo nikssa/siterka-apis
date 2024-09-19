8ba2fe66ccc15c3cf0a97d120b27471890955a99
nikssa <nikssa@gmail.com>
2024-09-19 02:07:16 +0200

    UI changes and experimenting

--- src/components/common/Hero/Hero.tsx
+++ src/components/common/Hero/Hero.tsx
@@ -1,17 +1,30 @@
-import Button from '@/components/formElements/Button/Button';
-import './Hero.scss';
 import Link from 'next/link';
+import Image from 'next/image';
+import SitterBabyInTheLaps from '../../../../public/pexels-vika-glitter-392079-1648363.webp';
+import CallToAction from './CallToAction';
+
+import './Hero.scss';
 
 const Hero = () => {
   return (
     <section className='hero main-hero'>
       <div className='inner'>
-        <div className='hero-image'></div>
-        <h1>Punchline here</h1>
-        <p>Lorem ipsum dolor sit amet</p>
-        <Link href='#' className='call-to-action btn'>
-          Call to action
-        </Link>
+        <div className='hero-image'>
+          <Image
+            src={SitterBabyInTheLaps}
+            alt='Sitter Baby in Lap'
+            placeholder='blur'
+            quality={50}
+            priority={true}
+            loading='eager'
+            layout='fill'
+          />
+        </div>
+        <div className='hero-text'>
+          <h1>Do you ever care enough?</h1>
+          <p>No matter what your needs are sitterka can help.</p>
+        </div>
+        <CallToAction />
       </div>
     </section>
   );
