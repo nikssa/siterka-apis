b4151b77f1da186e20f38d794281944133b889fb
nikssa <nikssa@gmail.com>
2025-02-05 15:07:18 +0100

    Adding single sitter job page

---
+++ src/app/(public)/sitter-jobs/[id]/page.tsx
@@ -0,0 +1,26 @@
+import UserCardDetails from '@/components/common/UserCardDetails/UserCardDetails';
+
+const SingleParentPage = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+
+  const userData = await fetch(`http://localhost:4000/api/sitters/${id}`, {
+    method: 'GET'
+  });
+
+  console.log('status', userData.status);
+  console.log('ok', userData.ok);
+
+  const user = await userData.json();
+
+  // TODO: Use user?.post?.title for SEO friendly URLs
+
+  return (
+    <section>
+      <div className='inner'>
+        <UserCardDetails data={user} />
+      </div>
+    </section>
+  );
+};
+
+export default SingleParentPage;
