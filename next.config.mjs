5a588341b04cc585edfd9622709a927f2becdff7
nikssa <nikssa@gmail.com>
2025-02-12 17:10:16 +0100

    update next config due to "images.domains" configuration being deprecated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'picsum.photos', 'localhost']
+    // The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.
+    // domains: ['images.pexels.com', 'picsum.photos', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**'
+      }
+    ]
   }
 };
 
